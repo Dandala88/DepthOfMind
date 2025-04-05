f50fa10aa18228cd1108cbc63b48da0d6cc54cd5
Daniel <33705402+Dandala88@users.noreply.github.com>
2025-04-05 13:05:30 -0400

    Add meditation stream

--- DepthOfMind/Program.cs
+++ DepthOfMind/Program.cs
@@ -1,82 +1,57 @@
 ﻿using System.Text;
 
-Console.SetCursorPosition(Console.WindowWidth / 2, 0);
-
-Console.WriteLine("Press Enter to meditate");
+Draw("Press Enter to meditate");
 var key = Console.ReadKey().Key;
 Console.Clear();
 Console.OutputEncoding = Encoding.UTF8;
 Console.CursorVisible = false;
-var depth = 0;
-var stream = "";
+var rnd = new Random();
 if (key == ConsoleKey.Enter)
 {
-    for(int i = 10; i > 0; i--)
-    {
-        var random = new Random();
-        var roll = random.Next(37, 1000);
-        Console.Beep((i + 1) * 100, 100);
-    }
-    var action = "";
+    //for (int i = 10; i > 0; i--)
+    //{
+    //    var random = new Random();
+    //    var roll = random.Next((i + 1) * 37, 1000);
+    //    Console.Beep(roll, 100);
+    //}
     var input = ConsoleKey.None;
-    while (action.ToLower() != "exit")
+    while (input != ConsoleKey.Enter)
     {
-        Console.WriteLine("Ohm.");
-        Console.WriteLine($"Depth: {depth}");
-        Console.WriteLine($"Stream: {stream}");
-        if (Console.KeyAvailable)
+        if(Console.KeyAvailable)
         {
             input = Console.ReadKey(true).Key;
         }
-        if(input == ConsoleKey.T)
-        {
-            Console.WriteLine("What are you thinking?");
-            var thought = Console.ReadLine();
-            if (string.IsNullOrEmpty(thought))
-            {
-                depth++;
-                Console.WriteLine("Clear mind is a deep mind.");
-            }
-            else
-            {
-                var total = 0;
-                for (int i = 0; i < thought.Length; i++)
-                {
-                    total += thought[i];
-                }
 
-                while (total > 999)
-                    total /= 2;
-                stream += (char)total;
-            }
-            Console.WriteLine("Press enter to continue.");
-            while (input != ConsoleKey.Enter)
-                input = Console.ReadKey(true).Key;
-            Console.Clear();
-        }
-        if(input == ConsoleKey.C)
-        {
-            var currentChar = 33;
-            while (input != ConsoleKey.Enter)
-            {
-                Console.Write((char)currentChar);
-                currentChar++;
-                if(currentChar > 50000)
-                    currentChar =33;
-                if (Console.KeyAvailable)
-                {
-                    input = Console.ReadKey(true).Key;
-                }
-                var cursorPosition = Console.GetCursorPosition();
-                Console.SetCursorPosition(0, cursorPosition.Top);
-            }
-            Console.WriteLine("Press enter to continue.");
-            while (input != ConsoleKey.Enter)
-                input = Console.ReadKey(true).Key;
-            Console.Clear();
-        }
+        if (input == ConsoleKey.C)
+            Console.SetCursorPosition(0, 0);
 
+        var total = rnd.Next(1, 91);
+        var nextStream = string.Empty;
+        for (int i = 0; i < total; i++)
+        {
+            var character = rnd.Next(33, 10000);
+            nextStream += (char)character;
+        }
+        var color = rnd.Next(0, 15);
+        Console.ForegroundColor = (ConsoleColor)color;
+        Draw(nextStream);
+        Console.SetCursorPosition(Console.GetCursorPosition().Left, Console.GetCursorPosition().Top - 1);
+        if (input == ConsoleKey.M)
+        {
+            Console.SetCursorPosition(Console.GetCursorPosition().Left, Console.GetCursorPosition().Top + 1);
+        }
+        else
+        {
+            Console.Write(new string(' ', Console.WindowWidth));
+        }
+        Thread.Sleep(100);
         input = ConsoleKey.None;
-        Console.SetCursorPosition(0, 0);
     }
+}
+
+void Draw(string text)
+{
+    var cursorPosition = Console.GetCursorPosition();
+    Console.SetCursorPosition(Console.WindowWidth / 2 - (text.Length / 2), cursorPosition.Top);
+    Console.WriteLine(text);
 }
